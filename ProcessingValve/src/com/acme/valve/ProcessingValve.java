a46625d09a80a8334605e8d1cab5b0b603aec399
Alexander Suter <alexander.suter@axonivy.com>
2020-08-31 09:54:02 +0200

    XIVY-2745 Define Log Level of logger directly in valve

--- ProcessingValve/src/com/acme/valve/ProcessingValve.java
+++ ProcessingValve/src/com/acme/valve/ProcessingValve.java
@@ -9,30 +9,37 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import ch.ivyteam.log.Logger;
 
 /**
- * Simple processing valve adapted so that it runs in the OSGi environment of Axon.ivy.
+ * Simple processing valve adapted so that it runs in the OSGi environment of
+ * Axon.ivy.
  * 
- * @see http://javaevangelist.blogspot.ch/2012/12/tomcat-7-custom-valve.html
+ * http://javaevangelist.blogspot.ch/2012/12/tomcat-7-custom-valve.html
  */
-public class ProcessingValve extends ValveBase{
-	
-	private static final Logger LOGGER = Logger.getLogger(ProcessingValve.class);
+public class ProcessingValve extends ValveBase
+{
+  private static final Logger LOGGER = Logger.getLogger(ProcessingValve.class);
 
-	@Override
-	public void invoke(Request request, Response response) throws IOException, ServletException {
-		HttpServletRequest httpServletRequest = request.getRequest();
-		 
-        Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
- 
-        while (headerNames.hasMoreElements()) {
-            String header = headerNames.nextElement();
-            LOGGER.info("Header --> {0} Value --> {1}", new Object[]{header, httpServletRequest.getHeader(header)});
-        }
- 
-        getNext().invoke(request, response);
-	}
+  static
+  {
+    Configurator.setLevel(LOGGER.getName(), Level.INFO);
+  }
 
+  @Override
+  public void invoke(Request request, Response response) throws IOException, ServletException
+  {
+    HttpServletRequest httpServletRequest = request.getRequest();
+    Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+    while (headerNames.hasMoreElements())
+    {
+      String header = headerNames.nextElement();
+      LOGGER.info("Header --> {0} Value --> {1}",
+              new Object[] {header, httpServletRequest.getHeader(header)});
+    }
+    getNext().invoke(request, response);
+  }
 }
